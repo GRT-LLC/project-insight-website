64c52435d58ac38883efa9fd13cc53ac65f62918
Ryan Pearson <ryan@ryanpearson.net>
2026-02-18 13:49:31 -0600

    removes unneseccary decorators from base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 export default defineConfig({
-  base: '/<project-insight-website>/',
+  base: '/project-insight-website/',
   plugins: [react()],
   resolve: {
     alias: {
