1d58334d04a2d1c295af90a8d7f233c297981e94
Ryan Pearson <ryan@ryanpearson.net>
2026-02-18 15:45:22 -0600

    Revert "Removes empty file"
    This reverts commit 824c9d84cc9e3553ee567692d8e6b7565b5544f1.

---
+++ src/marketing/router/RouterContext.tsx
@@ -0,0 +1,3 @@
+// This file is kept for backward compatibility but now uses React Router
+// Components should use useNavigate, useLocation from react-router-dom instead
+export { useAuth } from '../context/AuthContext';
